a2ab3d88ba3fecfc8d762155a41c45c091aadddc
JaiKumar78 <jaikumard7871@gmail.com>
2025-05-21 20:01:09 +0530

    script updates

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -7,7 +7,6 @@ import themedecicon from '../assets/themedecicon.png'
 import cameraicon from '../assets/photographyicon.png'
 import balloons from '../assets/balloons.png'
 import whatsappicon from '../assets/whatsappicon.png'
-import bgpattern from '../assets/bg-pattern.jpeg'
 import partyimg from '../assets/partyimg.jpeg'
 import themedecimg from '../assets/themedecimg.jpeg'
 import eventservicesimg from '../assets/eventservicesimg.jpeg'
@@ -16,22 +15,11 @@ import ceremonyimg from '../assets/ceremonyimg.jpeg'
 import decorationimg from '../assets/decorationimg.jpeg'
 import serviceherobg from '../assets/stagedecorationsrotated.jpeg'
 import stagedecorations2 from '../assets/stagedecorations2.jpeg'
-import stagedecorations3 from '../assets/stagedecorations3.jpg'
-import stagedecorations4 from '../assets/stagedecorations4.jpg'
 import stagedecorations5 from '../assets/stagedecorations5.jpg'
 import hero1 from '../assets/homehero1.jpeg'
-import hero3 from '../assets/homehero3.jpeg'
 import hero2 from '../assets/homehero2.jpeg'
 import about1 from '../assets/homeabout1.jpeg'
-import about2 from '../assets/homeabout2.jpeg'
-import sliderimg1 from '../assets/sliderimg1.jpeg'
-import sliderimg2 from '../assets/sliderimg2.jpeg'
-import sliderimg3 from '../assets/sliderimg3.jpeg'
-import sliderimg4 from '../assets/sliderimg4.jpg'
-import sliderimg5 from '../assets/sliderimg5.jpg'
-import pinkstage from '../assets/pinkstage.jpeg'
 import experiencebg from '../assets/experiencebg.jpeg'
-import facebook from '../assets/facebook.png'
 import whatsapp from '../assets/whatsapp.png'
 import youtube from '../assets/youtube.png'
 import instagram from '../assets/instagram.png'
@@ -60,7 +48,6 @@ const assets = {
     cameraicon,
     balloons,
     whatsappicon,
-    bgpattern,
     partyimg,
     themedecimg,
     eventservicesimg,
@@ -69,22 +56,11 @@ const assets = {
     serviceherobg,
     hero1,
     hero2,
-    hero3,
     about1,
-    about2,
-    sliderimg1,
-    sliderimg2,
-    sliderimg3,
-    sliderimg4,
-    sliderimg5,
     stagedecorations2,
-    stagedecorations3,
-    stagedecorations4,
     stagedecorations5,
-    pinkstage,
     experiencebg,
     whatsapp,
-    facebook,
     instagram,
     youtube,
     logo,
